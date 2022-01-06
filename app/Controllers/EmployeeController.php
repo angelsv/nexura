ec6e011e6dde2a7fada7a87a3da9d1ffefae9692
Angel Silva Valencia <asilva@micrositios.net>
2022-01-06 13:55:36 -0500

    fix(dao): Incluye elimiar y mejora proceso de guardado

--- app/Controllers/EmployeeController.php
+++ app/Controllers/EmployeeController.php
@@ -14,11 +14,13 @@ class EmployeeController extends Controller
 
     private $areas;
     private $roles;
+    private $employeeDao;
 
     function __construct(){
         parent::__construct();
         $areaModel = new AreaDao;
         $roleModel = new RoleDao;
+        $this->employeeDao = new EmployeeDao;
         $this->areas = $areaModel->getAll();
         $this->roles = $roleModel->getAll();
     }
@@ -27,35 +29,50 @@ class EmployeeController extends Controller
      * Cargar el formulario de creación/edición
      */
     function showForm(){
-        $this->template->twig->display('form.html', ['areas' => $this->areas, 'roles' => $this->roles]);
+        $this->template->twig->display('form.html', ['areas' => $this->areas, 'roles' => $this->roles, 'action' => '/employee']);
     }
 
     /**
      * Cargar la lista de empleados
      */
-    function getAll(){
-        $employeesModel = new EmployeeDao;
-        $employees = $employeesModel->getAll();
-        $this->template->twig->display('list.html', ['employees' => $employees]);
+    function getAll($data = []){
+        $employees = $this->employeeDao->getAll();
+        $data['employees'] = $employees;
+        $this->template->twig->display('list.html', $data);
     }
     
     /**
      * Cargar un empleado por su id
      */
     function getById($id){
-        $employeesModel = new EmployeeDao;
         $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-        $employee = $employeesModel->getById((int)$id);
+        $employee = $this->employeeDao->getById((int)$id);
         if( empty($employee) ){
             // TODO: implementar función error404
             echo 'error404';
             // exit;
         }
-        $this->template->twig->display('form.html', ['employee' => $employee, 'areas' => $this->areas, 'roles' => $this->roles]);
+        $this->template->twig->display('form.html', ['employee' => $employee, 'areas' => $this->areas, 'roles' => $this->roles, 'action' => "/employee/{$id}"]);
     }
 
+    /**
+     * Eliminar un registro
+     */
+    function delete(){
+        $id = filter_var(trim($_POST['id']), FILTER_SANITIZE_NUMBER_INT);
+        $res = $this->employeeDao->delete((int)$id);
+        $response = [
+            'response' => $res,
+            'msg' => $res ? 'Registro eliminado correctamente!' : 'Ocurrió un error eliminado el registro',
+        ];
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($response);
+    }
+
+    /**
+     * Proceso para guardar el registro
+     */
     function save(){
-        $employeeDao = new EmployeeDao;
 
         $name = filter_var(trim($_POST['employee']['nombre']), FILTER_SANITIZE_STRING);
         $email = filter_var(trim($_POST['employee']['email']), FILTER_SANITIZE_EMAIL);
@@ -73,18 +90,19 @@ class EmployeeController extends Controller
         $employeeDto->setAreaId((int)$area);
         $employeeDto->setBoletin((int)$newsletter);
         $employeeDto->setDescripcion($description);
-        // $employeeDto->setRole($role);
 
         // Procesar creación/actualización
-        if( isset($_POST['employee']['id']) && !empty($employeeDao->getById((int)$_POST['employee']['id'])) ){
-            $employeeDto->setId((int)$_POST['employee']['id']);
-            $employee = $employeeDao->update($employeeDto);
+        $exists_user = isset($_POST['employee']['id']) && !empty($this->employeeDao->getById((int)$_POST['employee']['id']));
+        if( $exists_user ){
+            $id = filter_var(trim($_POST['employee']['id']), FILTER_SANITIZE_NUMBER_INT);
+            $employeeDto->setId((int)$id);
+            $employee = $this->employeeDao->update($employeeDto);
             $response = [
                 'response' => !empty($employee),
                 'msg' => !empty($employee) ? 'Registro actualizado satisfactoriamente!' : 'Error actualizando el registro',
             ];
         }else{
-            $employee = $employeeDao->insert($employeeDto);
+            $employee = $this->employeeDao->insert($employeeDto);
             $response = [
                 'response' => !empty($employee),
                 'msg' => !empty($employee) ? 'Registro creado satisfactoriamente!' : 'Error creando el registro',
@@ -95,10 +113,14 @@ class EmployeeController extends Controller
         if( $response['response'] ){
             $roles = $_POST['employee']['roles'];
             $employeeRoleDao = new EmployeeRoleDao;
-            $employee['roles'] = $employeeRoleDao->insertRolesByEmployeeId((int)$_POST['employee']['id'], $roles);
+            $employee['roles'] = $employeeRoleDao->insertRolesByEmployeeId((int)$employee['id'], $roles);
         }
 
-        $this->template->twig->display('form.html', ['employee' => $employee , 'response' => $response, 'areas' => $this->areas, 'roles' => $this->roles]);
+        if( $exists_user ){
+            $this->template->twig->display('form.html', ['employee' => $employee , 'response' => $response, 'areas' => $this->areas, 'roles' => $this->roles, 'action' => "/employee/{$id}"]);
+        }else{
+            $this->getAll(['response' => $response]);
+        }
 
     }
 
